105c9a94aaa234fbb8bd7841a0b5d4dd1d9dc19f
rockspetre <rockspetre@gmail.com>
2019-09-30 18:41:03 +0100

    Update videoconvert.js

--- routes/videoconvert.js
+++ routes/videoconvert.js
@@ -1,1 +1,26 @@
+//THIS MERGES MULTIPLE VIDEO FILES
 
+var firstFile = "title.mp4";
+var secondFile = "source.mp4";
+var thirdFile = "third.mov";
+var outPath = "out.mp4";
+
+//the above listed sources can be file input streams 
+//NOTE  you cannot chain mulitple file input streams with the FFMPEG api
+//figure it out **winks**
+
+//E.G 
+//var newFile = require("fs").readFileSync('/path-to-file');
+
+var proc = ffmpeg(firstFile)
+    .input(secondFile)
+    .input(thirdFile)
+    //.input(fourthFile)
+    //.input(...)
+    .on('end', function() {
+      console.log('files have been merged succesfully');
+    })
+    .on('error', function(err) {
+      console.log('an error happened: ' + err.message);
+    })
+    .mergeToFile(outPath);
